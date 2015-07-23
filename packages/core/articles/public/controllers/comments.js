8fd98b7985e571a2dae7a88c2ead0da018db00bb
Mohamed Amin <mohamed.amin.088@gmail.com>
2015-07-24 01:37:55 +0400

    Enhancing the Comments service mocking

--- packages/core/articles/public/controllers/comments.js
+++ packages/core/articles/public/controllers/comments.js
@@ -4,5 +4,12 @@ angular.module('mean.articles').controller('CommentsController', ['$scope', '$st
   function($scope, $stateParams, $location, Global, Comments) {
     $scope.comments = Comments;
     console.log(Comments)
+    $scope.create = function(isValid) {
+      if (isValid) {
+        console.log('Sending the Comment the API')
+      } else {
+        $scope.submitted = true;
+      }
+    };
   }
 ]);
--- packages/core/articles/public/services/comments.js
+++ packages/core/articles/public/services/comments.js
@@ -1,38 +1,53 @@
 'use strict';
-
-// //Comments  service used for comments REST endpoint
-// angular.module('mean.articles').factory('Comments', ['$resource',
-//   function($resource) {
-//     return $resource('api/articles/:articleId', {
-//       articleId: '@_id'
-//     }, {
-//       update: {
-//         method: 'PUT'
-//       }
-//     });
-//   }
-// ]);
-
+/**
+* Mocking for comments retrieving service 
+*/
 angular.module('mean.articles').factory('Comments', [
-  function() {
-    return [{
-            id: '1',
-            createdAt: '20:20:20:20',
-            status: 'PENDING',
-            content: 'Test test test test test',
-            user: 'Amin'
-        }, {
-            id: '1',
-            createdAt: '20:20:20:20',
-            status: 'PENDING',
-            content: 'Test test test test test',
-            user: 'Amin'
-        }, {
-            id: '1',
-            createdAt: '20:20:20:20',
-            status: 'PENDING',
-            content: 'Test test test test test',
-            user: 'Amin'
-        }]
-  }
+    function() {
+        var Comments = this;
+        Comments.getPosts = function(status) {
+            if ( status === 'PENDING') {
+            	return [{
+		            id: '1',
+		            createdAt: '20:20:20:20',
+		            status: 'PENDING',
+		            content: 'Test test test test test',
+		            user: 'Amin'
+		        }, {
+		            id: '1',
+		            createdAt: '20:20:20:20',
+		            status: 'PENDING',
+		            content: 'Test test test test test',
+		            user: 'Amin'
+		        }, {
+		            id: '1',
+		            createdAt: '20:20:20:20',
+		            status: 'PENDING',
+		            content: 'Test test test test test',
+		            user: 'Amin'
+		        }]
+            } else if ( status === 'APPROVED') {
+            	return [{
+		            id: '1',
+		            createdAt: '20:20:20:20',
+		            status: 'APPROVED',
+		            content: 'Test test test test test',
+		            user: 'Amin'
+		        }, {
+		            id: '1',
+		            createdAt: '20:20:20:20',
+		            status: 'APPROVED',
+		            content: 'Test test test test test',
+		            user: 'Amin'
+		        }, {
+		            id: '1',
+		            createdAt: '20:20:20:20',
+		            status: 'APPROVED',
+		            content: 'Test test test test test',
+		            user: 'Amin'
+		        }]
+            }
+        };
+        
+    }
 ]);
